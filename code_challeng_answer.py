69b1f1ec970669e57f63c5b179b1ee5a4724051a
wenjie <william_y@outlook.com>
2023-02-03 16:23:06 -0500

    check with status code and add test case for per_page specific limit

--- code_challeng_answer.py
+++ code_challeng_answer.py
@@ -6,52 +6,107 @@ class TestDiscogs(unittest.TestCase):
     # valid key
     key = "sEfcNKVvbrdRSENJaWSy"
     # invalid key
-    #key = "EfcNKVvbrdRSENJaWSy"
-    
+    key_invalid = "EfcNKVvbrdRSENJaWSy"
+
     secret = "vlyhhCzDfReNaOaNSBzRwCoTAVRPxODt"
     discog_url = "https://api.discogs.com/database"
-    
+
+    defaut_per_page = 50
+    specific_limit_per_page = 20
+    max_per_page = 100
+    over_max_per_page = 110
+      
+
     @classmethod
     def setUpClass(self):
         auth = "Discogs key={}, secret={}".format(self.key, self.secret)
+        auth_invalid = "Discogs key={}, secret={}".format(self.key_invalid, self.secret)
         self.headers = {
             "Content-Type": "application/x-www-form-urlencoded",
             "Authorization": auth
             }
-    # check Access success or not 
-    def test_basic_response(self):
-        res = requests.get("{}/search".format(self.discog_url), headers=self.headers)
- 
-        print(res.request)
-        print(res.json())
-        jsondata = res.json()
 
-        #check if there is "message" key in the res.json() if yes then access failed otherwise access succeed 
-        if"message" in jsondata:
-            access = False
-        else:  
-            access = True
-        
-        #asserting
-        self.assertTrue(access)
-
-    # If no limit specified, we return default number of record per page
+        self.headers_invalid = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": auth_invalid
+            }
     
-    def test_default_Pagination(self):
-        res = requests.get("{}/search".format(self.discog_url), headers=self.headers)
+        #If a specific limit is specified, the api must return the requested number of record per pages.  
+        self.pagni_param_specific_limit ={
+            "per_page": self.specific_limit_per_page
+
+            }
+       #The api will return a maximun number of result per pages, even if we provide a large limit value.     
+        self.pagni_param_over_max ={
+            "per_page": self.over_max_per_page
+            }
+
+    def test_basic_response(self):
+        res = requests.get("{}/search".format(self.discog_url),headers=self.headers)
  
         print(res.request)
         print(res.json())
-        jsondata = res.json()
-
+    
+        if res.status_code==200:
+            access = True
+        else:
+            access = False    
+        res = res.json()
         jsonpath_expression = parse("$.pagination.per_page")
 
-        for match in jsonpath_expression.find(jsondata):
+        for match in jsonpath_expression.find(res):
 	         per_page_num = match.value
         
-        #asserting
-        self.assertEqual(per_page_num,50)
+        #asserting access success
+        self.assertTrue(access)
+
+        #asserting default per page without limit specified
+        self.assertEqual(per_page_num,self.defaut_per_page)
+
+       
+
+    def test_basic_response_fail(self):
+        res = requests.get("{}/search".format(self.discog_url),headers=self.headers_invalid)
+ 
+        print(res.request)
+        print(res.json())
+    
+        if res.status_code==401:
+            access = True
+        else:
+            access = False    
+        #asserting access no success
+
+        self.assertTrue(access)   
+
+    def test_pagni_response_limit_specific(self):
+        res = requests.get("{}/search".format(self.discog_url), params =self.pagni_param_specific_limit, headers=self.headers)
+ 
+        print(res.request)
+        print(res.json())
+        res = res.json()
+        jsonpath_expression = parse("$.pagination.per_page")
+
+        for match in jsonpath_expression.find(res):
+	         per_page_num = match.value
         
+        #asserting per page with specific limit
+
+        self.assertEqual(per_page_num,self.specific_limit_per_page)
+    
+
+    def test_pagni_response_over_max(self):
+        res = requests.get("{}/search".format(self.discog_url), params =self.pagni_param_over_max, headers=self.headers)
+ 
+        print(res.request)
+        print(res.json())
+        res = res.json()
+        jsonpath_expression = parse("$.pagination.per_page")
+
+        for match in jsonpath_expression.find(res):
+	         per_page_num = match.value
+        #asserting max per_page with limit over max specified     
+        self.assertEqual(per_page_num,self.max_per_page)
 
 if __name__ == '__main__':
     #unittest.main()
